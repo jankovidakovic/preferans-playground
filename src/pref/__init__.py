8dcef8b105b95d02c80a83d84b6b563bcfa985d3
jankovidakovic <janko@tensorpix.ai>
2026-08-07 01:30:19 +0200

    feat: add argparse

--- src/pref/__init__.py
+++ src/pref/__init__.py
@@ -1,10 +1,18 @@
+import argparse
+
 from pref.game import play_game
 
 
-def main() -> None:
-    results = play_game(starting_points=30, max_rounds=10)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--starting_points", type=int, default=30)
+    parser.add_argument("--max_rounds", type=int, default=10)
+
+
+def main(starting_points: int, max_rounds: int) -> None:
+    results = play_game(starting_points, max_rounds)
     print(results)
 
 
 if __name__ == "__main__":
-    main()
+    main(**vars(parse_args()))
